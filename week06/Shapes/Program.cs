1d0ce4cb8aa3fc52c8ea5357322705ddbbb0aea8
sasharity <iruogheneomaraye@gmail.com>
2025-02-15 13:54:04 +0100

    Shape program using polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 8);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 5, 10);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("White", 7);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            // Notice that all shapes have a GetColor method from the base class
+            string color = s.GetColor();
+
+            // Notice that all shapes have a GetArea method, but the behavior is
+            // different for each type of shape
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
